b7b373c1c3fa20987fcce9fd2837accc81074f08
olrlobt <317tmdgjs@naver.com>
2023-11-12 18:39:51 +0900

    fix: 게시판 날짜 파싱오류 수정

--- enjoytrip_java_last/src/main/java/com/ssafy/ux/finalpj/board/controller/BoardController2.java
+++ enjoytrip_java_last/src/main/java/com/ssafy/ux/finalpj/board/controller/BoardController2.java
@@ -3,20 +3,21 @@ package com.ssafy.ux.finalpj.board.controller;
 import com.ssafy.ux.finalpj.board.model.BoardDto;
 import com.ssafy.ux.finalpj.board.model.service.BoardService;
 import com.ssafy.ux.finalpj.member.model.MemberDto;
+import java.util.List;
+import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-
-import org.apache.ibatis.annotations.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ConcurrentModel;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
--- enjoytrip_java_last/src/main/java/com/ssafy/ux/finalpj/board/model/BoardDto.java
+++ enjoytrip_java_last/src/main/java/com/ssafy/ux/finalpj/board/model/BoardDto.java
@@ -1,9 +1,11 @@
 package com.ssafy.ux.finalpj.board.model;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import lombok.Data;
 
 @Data
@@ -16,24 +18,5 @@ public class BoardDto {
     private int hit;
     private String registerTime;
     private String type;
-
-    public void setRegisterTime(String registerTime) {
-        if (registerTime != null && !registerTime.isEmpty()) {
-            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-            SimpleDateFormat outputFormat = new SimpleDateFormat("yy/MM/dd HH:mm");
-
-            try {
-                Date date = inputFormat.parse(registerTime);
-                this.registerTime = outputFormat.format(date);
-            } catch (ParseException e) {
-                e.printStackTrace();
-                // Handle the parsing exception, log it, or throw a specific exception
-            }
-        } else {
-            // Handle the case where registerTime is empty
-            // You can set a default value or take appropriate action
-            this.registerTime = null; // or set a default time
-        }
-    }
 
 }
