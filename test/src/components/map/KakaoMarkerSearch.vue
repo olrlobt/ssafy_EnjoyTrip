949e268ccbc66d0c440cd3179481457c1547aa28
olrlobt <317tmdgjs@naver.com>
2023-11-16 08:34:42 +0900

    feat: 관광지 디테일 틀 추가

--- test/src/components/map/KakaoMarkerSearch.vue
+++ test/src/components/map/KakaoMarkerSearch.vue
@@ -1,5 +1,99 @@
+<template>
+  <div class="window2">
+    <div class="head">
+      <div class="buttons">
+        <div class="button -red"></div>
+        <div class="button -yellow"></div>
+        <div class="button -green"></div>
+      </div>
+      <div class="tabs-group">
+
+        <div class="tabs"  :style="tabsWidth">
+          <div
+              :class="[active(1), 'tab']"
+              @mousedown="changeTab(1)"
+          >
+            <span class="text"> 관광 타입 선택 </span>
+            <div
+                class="close-button"
+            ></div>
+          </div>
+
+
+        </div>
+
+
+        <div key="add-button" class="add-button" @click="addTab"></div>
+      </div>
+    </div>
+    <div class="menu">
+      <div class="buttons">
+        <div class="arrows">
+          <div class="arrow -back"></div>
+          <div class="arrow -forward"></div>
+        </div>
+      </div>
+      <input class="search" type="text"  id="keyword" placeholder="지역/상호명 검색" @keyup="searchKeyword" />
+    </div>
+
+  </div>
+</template>
 <script setup>
-import {defineProps, onMounted, ref} from "vue";
+import { ref, reactive, computed, onMounted } from 'vue';
+import {useMapStore} from "../../stores/map";
+
+const mapStore = useMapStore();
+const currentTabId = ref(1);
+const nextTabId = ref(4);
+
+const tabs = reactive([
+  {
+    id: 1,
+    name: "여행 경로",
+  },
+  {
+    id: 2,
+    name: "관광지 리스트",
+  }
+]);
+
+
+onMounted(() => {
+
+})
+
+const active = (id) => id === currentTabId.value ? "-active" : "";
+
+
+
+const tabsWidth = computed(() => {
+  return {
+    maxWidth: tabs.length * 190 + "px",
+    width: "100%",
+    minWidth: "0px",
+    transition: "max-width 0.2s",
+  };
+});
+
+function changeTab(id) {
+  currentTabId.value = id;
+}
+
+
+function addTab() {
+  tabs.push({
+    id: nextTabId.value,
+    name: "new tab"
+  });
+  currentTabId.value = nextTabId.value;
+  nextTabId.value++;
+}
+
+
+
+
+
+
 
 
 const { VITE_TRAVEL_API_KEY } = import.meta.env;
@@ -59,7 +153,7 @@ const searchKeyword = (event) => {
     serviceKey: VITE_TRAVEL_API_KEY,
     _type: 'json'
   };
-    console.log(params.keyword)
+  console.log(params.keyword)
   searchWithDebounce(params, "searchKeyword1");
 }
 
@@ -122,7 +216,7 @@ function callAPI(params, keyword_search) {
       })
       .catch(error => {
         console.error(error);
-  });
+      });
 
 
   function getMapCoordinates(data) {
@@ -217,6 +311,10 @@ function callAPI(params, keyword_search) {
   function handleMarkerClick(marker, coord) {
     const isFixed = fixedMarkers.value.includes(marker);
 
+    mapStore.detailState = coord;
+
+    console.log(mapStore.detailState);
+
     // 인포윈도우 업데이트 로직
     const content = generateInfoWindowContent(coord, isFixed);
     infoWindow.value.setContent(content);
@@ -307,7 +405,7 @@ function callAPI(params, keyword_search) {
     fixedMarkerPositions.value.push(marker.getPosition());
 
     const redMarkerImage = new kakao.maps.MarkerImage(
-        '${root}/assets/img/pickMarker.png',
+        '${root}/assets/imgages/pickMarker.png',
         new kakao.maps.Size(40, 40)
     );
 
@@ -344,43 +442,332 @@ function callAPI(params, keyword_search) {
   }
 
 
-  document.getElementById('travelRouteList').addEventListener('click', function (event) {
-    if (event.target.className === 'deleteButton') {  // 'deleteButton' 클래스를 가진 버튼을 클릭했을 때만 동작
-      const listItem = event.target.closest('li');   // 삭제 버튼의 부모 li 요소를 찾습니다.
-      const markerTitle = listItem.getAttribute('data-title');
-
-      // fixedMarkers 배열에서 해당 제목과 일치하는 마커를 찾습니다.
-      const targetMarker = fixedMarkers.value.find(marker => marker.getTitle() === markerTitle);
-      if (targetMarker) {
-        revertMarker(targetMarker); // 해당 마커의 고정을 해제합니다.
-      }
-
-      // 목록에서 해당 항목을 제거합니다.
-      listItem.remove();
-    }
-  });
+  // document.getElementById('travelRouteList').addEventListener('click', function (event) {
+  //   if (event.target.className === 'deleteButton') {  // 'deleteButton' 클래스를 가진 버튼을 클릭했을 때만 동작
+  //     const listItem = event.target.closest('li');   // 삭제 버튼의 부모 li 요소를 찾습니다.
+  //     const markerTitle = listItem.getAttribute('data-title');
+  //
+  //     // fixedMarkers 배열에서 해당 제목과 일치하는 마커를 찾습니다.
+  //     const targetMarker = fixedMarkers.value.find(marker => marker.getTitle() === markerTitle);
+  //     if (targetMarker) {
+  //       revertMarker(targetMarker); // 해당 마커의 고정을 해제합니다.
+  //     }
+  //
+  //     // 목록에서 해당 항목을 제거합니다.
+  //     listItem.remove();
+  //   }
+  // });
 }
+
+
 
 </script>
 
-<template>
 
-  <div class="control-container map-popup">
-    <select id="tourismType ">
-      <option value="12" selected>관광타입 선택</option>
-      <option value="12">관광지</option>
-      <option value="14">문화시설</option>
-      <option value="15">축제공연행사</option>
-      <option value="25">여행코스</option>
-      <option value="28">레포츠</option>
-      <option value="32">숙박</option>
-      <option value="38">쇼핑</option>
-      <option value="39">음식점</option>
-    </select>
-    <div class="search-container">
-      <input type="text" class="search-input" id="keyword" placeholder="지역/상호명" @keyup="searchKeyword">
-      <button class="search-button">검색</button>
-    </div>
-  </div>
+<style lang="scss">
+$white: #fff;
+$gray-dark: #dee1e6;
+$gray-light: #e7eaef;
 
-</template>
+//body {
+//  padding: 20px;
+//  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
+//  display: flex;
+//  align-items: center;
+//  height: 100vh;
+//  box-sizing: border-box;
+//  background: linear-gradient(135deg, rgb(250,250,255) 0%, rgb(240,240,246) 50%, rgb(230,230,237) 100%);
+//}
+
+.window2 {
+  position: absolute;
+  z-index: 3;
+  top: 20px;
+  right: 30px;
+  width: 30%;
+  max-width: 350px;
+  max-height: 20%;
+  height: 15%;
+
+  border-radius: 8px;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 4px 8px 20px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+
+  > .content {
+    min-height: 0;
+    height: 100%;
+  }
+}
+
+.head {
+  background-color: $gray-dark;
+  display: flex;
+  overflow: hidden;
+
+  .buttons {
+    display: flex;
+    align-items: center;
+    padding: 0 4px 0 12px;
+
+    > .button {
+      width: 12px;
+      height: 12px;
+      border-radius: 100%;
+
+      &:not(:last-child) {
+        margin-right: 6px;
+      }
+
+      &.-red {
+        background-color: #fb5a59;
+      }
+
+      &.-yellow {
+        background-color: #ffc139;
+      }
+
+      &.-green {
+        background-color: #60d045;
+      }
+    }
+  }
+}
+
+.menu {
+  min-height: 36px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid $gray-dark;
+
+  > .buttons {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 14px;
+
+    > .arrows {
+      display: flex;
+      justify-content: space-between;
+      width: 44px;
+
+      > .arrow {
+        width: 14px;
+        height: 2px;
+        background-color: #ccc;
+        border-radius: 100px;
+        position: relative;
+
+        &::before,
+        &::after {
+          content: "";
+          display: block;
+          background-color: #ccc;
+          position: absolute;
+          width: 71%;
+          height: 100%;
+          border-radius: 100px;
+          transform-origin: center left;
+        }
+
+        &::before {
+          transform: rotate(45deg);
+        }
+
+        &::after {
+          transform: rotate(-45deg);
+        }
+
+        &.-forward {
+          transform: rotate(180deg);
+        }
+      }
+    }
+  }
+
+  > .search {
+    box-sizing: border-box;
+    border-radius: 100px;
+    border: 0;
+    background-color: #f1f1f1;
+    height: 26px;
+    outline: 0;
+    flex: 1;
+    margin-right: 20px;
+    padding: 0 12px;
+    font-size: 12px;
+    border: 2px solid transparent;
+
+    &:focus {
+      border: 2px solid #6dbce7;
+    }
+  }
+}
+
+.tabs-group {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+  min-width: 0px;
+
+  > .add-button {
+    width: 14px;
+    height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-self: center;
+    padding: 8px;
+    margin: 0 4px 2px;
+    border-radius: 100%;
+    transition: background-color 0.5s;
+
+    &:hover {
+      background-color: #ccc;
+    }
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 2px;
+      height: 14px;
+      background-color: #666;
+    }
+
+    &::before {
+      transform: rotate(90deg);
+    }
+  }
+}
+
+.tabs {
+  display: inline-flex;
+  align-items: flex-end;
+  box-sizing: border-box;
+  padding: 0 8px;
+  line-height: 1;
+  position: relative;
+  z-index: 5;
+  user-select: none;
+  width: 100%;
+}
+
+.tab {
+  position: relative;
+  background-color: transparent;
+  padding: 14px 12px;
+  border-radius: 10px 10px 0 0;
+  font-size: 12px;
+  display: block;
+  max-width: 160px;
+  transition: background-color 0.2s;
+  cursor: default;
+  position: relative;
+  margin-right: -1px;
+  white-space: nowrap;
+  flex: 1;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &:hover {
+    background-color: $gray-light;
+
+    &::before {
+      background-color: $gray-light;
+      right: 0;
+    }
+  }
+
+  &.-active {
+    background-color: $white;
+
+    &::before {
+      background-color: $white;
+      right: 0;
+    }
+  }
+
+  &.-dragging {
+    opacity: 0;
+  }
+
+  &::after {
+    content: "";
+    display: block;
+    height: 20px;
+    width: 1px;
+    background-color: #888;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+    z-index: -1;
+  }
+
+  &::before {
+    content: "";
+    display: block;
+    height: 20px;
+    width: 30px;
+    position: absolute;
+    right: 1px;
+    background-color: $gray-dark;
+    transition: background-color 0.2s;
+  }
+
+  > .close-button {
+    width: 10px;
+    height: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: rotate(45deg);
+    border-radius: 100%;
+    padding: 4px;
+    position: absolute;
+    right: 8px;
+
+    &:hover {
+      background-color: #ddd;
+    }
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 2px;
+      height: 12px;
+      background-color: #666;
+    }
+
+    &::before {
+      transform: rotate(90deg);
+    }
+  }
+}
+
+.tab-content {
+  display: none;
+  padding: 20px;
+  height: 100%;
+  box-sizing: border-box;
+
+  &.-active {
+    display: block;
+  }
+
+  > .image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
