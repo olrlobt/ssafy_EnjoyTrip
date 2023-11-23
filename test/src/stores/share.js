66b6e38e3a55db7b4a018372e5ee486c75f70b9f
youkyoungJung <yyk834@naver.com>
2023-11-24 03:20:41 +0900

    feat: 메인화면 & 조회수올리기 구현

--- test/src/stores/share.js
+++ test/src/stores/share.js
@@ -1,12 +1,27 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import { listTop6TravelRoute } from '@/api/share';
 
 
 export const useShareStore = defineStore("shareStore", () => {
 
     const travelRoute = ref({});
+    const top6TravelRoutes = ref([]);
+
+    const getTop6TravelRoutes = async () => {
+        try {
+            const data = await listTop6TravelRoute();
+            console.log(data);
+            top6TravelRoutes.value = data;
+        } catch (error) {
+            console.error('Top 6 여행 경로를 가져오는 중 에러 발생:', error);
+        }
+    };
 
     return {
-        travelRoute
+        travelRoute,
+        top6TravelRoutes,
+        getTop6TravelRoutes,
+
     };
 });
